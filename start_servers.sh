c860ba3087217f16ab5a669b4362f7ebb886ec3a
EC2 Default User <ec2-user@ip-172-31-23-67.ec2.internal>
2025-09-05 13:45:03 +0000

    tty

--- start_servers.sh
+++ start_servers.sh
@@ -6,13 +6,13 @@ pip3 install flask flask-cors requests boto3
 
 # 백엔드 서버 (imsi_new.py) 실행
 echo "Starting backend server (imsi_new.py) on port 5000..."
-cd /home/ec2-user
+cd /home/ec2-user/team02-aws-hackathon
 nohup python3 imsi_new.py > backend.log 2>&1 &
 BACKEND_PID=$!
 
 # 프론트엔드 서버 (front/app.py) 실행
 echo "Starting frontend server on port 8080..."
-cd /home/ec2-user/front
+cd /home/ec2-user/team02-aws-hackathon/front
 nohup python3 app.py > frontend.log 2>&1 &
 FRONTEND_PID=$!
 
